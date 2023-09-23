65b96735be607e8c0fcfd2be5291d11f08c44d71
jannoh <annohjoseph5@gmail.com>
2023-09-23 10:08:13 +0000

    Understand Terraform Settings & Providers blocks

---
+++ aws_eks/03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -0,0 +1,26 @@
+#Terraform Block
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+#Provider Block
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "vscode"
+}
+
+
+
+
+
+/*
+Note-1: AWS Credentials Profile (profile = "default") configured on your local desktop terminal
+~/.aws/credentials
+*/
